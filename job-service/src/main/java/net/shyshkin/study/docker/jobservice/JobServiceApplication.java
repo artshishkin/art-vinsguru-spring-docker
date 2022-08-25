b4a5560b0c01e0705082852563109b4f6b193f70
Art <d.art.shishkin@gmail.com>
2022-08-25 21:13:14 +0300

    66.1 Project Setup (#3 Section 6: Dockerizing Spring Application)

---
+++ job-service/src/main/java/net/shyshkin/study/docker/jobservice/JobServiceApplication.java
@@ -0,0 +1,12 @@
+package net.shyshkin.study.docker.jobservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobServiceApplication.class, args);
+    }
+}
---
+++ job-service/src/test/java/net/shyshkin/study/docker/jobservice/JobServiceApplicationTest.java
@@ -0,0 +1,13 @@
+package net.shyshkin.study.docker.jobservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobServiceApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
