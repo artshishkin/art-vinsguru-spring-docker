782185a17cb29b3d38a32da8213065dce1283d92
Art <d.art.shishkin@gmail.com>
2022-09-01 18:13:34 +0300

    86. Adding Logback XML (#4)

--- job-service/src/test/java/net/shyshkin/study/docker/jobservice/JobServiceApplicationIT.java
+++ job-service/src/test/java/net/shyshkin/study/docker/jobservice/JobServiceApplicationIT.java
@@ -1,5 +1,6 @@
 package net.shyshkin.study.docker.jobservice;
 
+import lombok.extern.slf4j.Slf4j;
 import net.shyshkin.study.docker.common.BaseTest;
 import net.shyshkin.study.docker.jobservice.dto.JobDto;
 import org.junit.jupiter.api.Test;
@@ -12,6 +13,7 @@ import org.springframework.test.web.reactive.server.WebTestClient;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Slf4j
 @SpringBootTest
 @AutoConfigureWebTestClient
 class JobServiceApplicationIT extends BaseTest {
@@ -32,6 +34,7 @@ class JobServiceApplicationIT extends BaseTest {
                 .expectStatus().isOk()
                 .expectBodyList(JobDto.class)
                 .hasSize(5)
+                .value(jobs -> log.debug("Result: {}", jobs))
                 .value(jobs -> assertThat(jobs)
                         .allSatisfy(job -> assertThat(job).hasNoNullFieldsOrProperties()))
         ;
