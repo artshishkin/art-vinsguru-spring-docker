0e6e84d25cd89c495d0b344791cb00b0b3042716
Art <d.art.shishkin@gmail.com>
2022-09-11 19:19:49 +0300

    8.5 Added Some Logging (#8)

--- candidate-service/src/main/java/net/shyshkin/study/docker/candidateservice/controller/CandidateController.java
+++ candidate-service/src/main/java/net/shyshkin/study/docker/candidateservice/controller/CandidateController.java
@@ -1,6 +1,7 @@
 package net.shyshkin.study.docker.candidateservice.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import net.shyshkin.study.docker.candidateservice.dto.CandidateDetailsDto;
 import net.shyshkin.study.docker.candidateservice.dto.CandidateDto;
 import net.shyshkin.study.docker.candidateservice.service.CandidateService;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 @RestController
 @RequestMapping("candidates")
 @RequiredArgsConstructor
@@ -17,17 +19,20 @@ public class CandidateController {
 
     @GetMapping
     public Flux<CandidateDto> getAllCandidates() {
+        log.info("Getting all candidates");
         return service.getAllCandidates();
     }
 
     @GetMapping("{id}")
     public Mono<CandidateDetailsDto> getCandidateById(@PathVariable String id) {
+        log.debug("Getting candidate by id: {}", id);
         return service.getCandidateById(id);
     }
 
     @PostMapping
     public Mono<CandidateDto> createCandidate(@RequestBody Mono<CandidateDto> dtoMono) {
-        return service.createCandidate(dtoMono);
+        return service.createCandidate(dtoMono)
+                .doOnNext(dto -> log.debug("Created new candidate: {}", dto));
     }
 
 }
--- job-service/src/main/java/net/shyshkin/study/docker/jobservice/controller/JobController.java
+++ job-service/src/main/java/net/shyshkin/study/docker/jobservice/controller/JobController.java
@@ -1,6 +1,7 @@
 package net.shyshkin.study.docker.jobservice.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import net.shyshkin.study.docker.jobservice.dto.JobDto;
 import net.shyshkin.study.docker.jobservice.service.JobService;
 import org.springframework.web.bind.annotation.*;
@@ -9,6 +10,7 @@ import reactor.core.publisher.Mono;
 
 import java.util.Set;
 
+@Slf4j
 @RestController
 @RequestMapping("jobs")
 @RequiredArgsConstructor
@@ -18,31 +20,38 @@ public class JobController {
 
     @GetMapping
     public Flux<JobDto> getAllJobs() {
+        log.info("Getting all the jobs");
         return jobService.allJobs();
     }
 
     @GetMapping(params = {"skills"})
     public Flux<JobDto> getJobsBySkills(@RequestParam("skills") Set<String> skills) {
+        log.debug("Getting jobs by skills");
         return jobService.jobsBySkills(skills);
     }
 
     @GetMapping("{id}")
     public Mono<JobDto> getJobById(@PathVariable String id) {
+        log.debug("Getting job by id");
         return jobService.getJob(id);
     }
 
     @PostMapping
     public Mono<JobDto> createNewJob(@RequestBody Mono<JobDto> jobDtoMono) {
-        return jobService.createNewJob(jobDtoMono);
+        return jobService.createNewJob(jobDtoMono)
+                .doOnNext(dto -> log.debug("Created new job {}", dto));
     }
 
     @PutMapping("{id}")
     public Mono<JobDto> updateJob(@PathVariable String id, @RequestBody Mono<JobDto> jobDtoMono) {
-        return jobService.updateJob(id, jobDtoMono);
+        log.debug("Start updating job with id: {}", id);
+        return jobService.updateJob(id, jobDtoMono)
+                .doOnNext(dto -> log.debug("Job updated: {}", dto));
     }
 
     @DeleteMapping("{id}")
     public Mono<Void> delete(@PathVariable String id) {
+        log.warn("Deleting job with id: {}", id);
         return jobService.delete(id);
     }
 
